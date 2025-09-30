25522f1a9845d858d29961bf96fd2db592802d57
M Anzal Javed <anzaljoffl@gmail.com>
2025-09-30 23:26:17 +0530

    Style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,14 @@ export default function()
     return(
         <>
         <nav>
-        <a href="#about">ABOUT</a>
-        <a href="#skills">SKILLS</a>
-        <a href="#achievements">ACHIEVEMENTS</a>
-        <a href="#projects">PROJECTS</a>
-        <a href="#contact">CONTACTS </a>
+            <h2>M Anzal Javed</h2>
+            <ul>
+                <li><a href="#about">ABOUT</a></li>
+                <li><a href="#skills">SKILLS</a></li>
+                <li><a href="#achievements">ACHIEVEMENTS</a></li>
+                <li><a href="#projects">PROJECTS</a></li>
+                <li><a href="#contact">CONTACTS </a></li>
+            </ul>
         </nav>
         </>
     )
